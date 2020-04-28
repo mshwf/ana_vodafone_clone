dc7747224667b1df2bc73c563be1be524b338225
Mohamed Elshawaf <m_shawaf@outlook.com>
2020-04-28 03:49:36 +0200

    voice search animation

--- lib/search_page.dart
+++ lib/search_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class SearchPage extends StatefulWidget {
@@ -5,7 +7,21 @@ class SearchPage extends StatefulWidget {
   _SearchPageState createState() => _SearchPageState();
 }
 
-class _SearchPageState extends State<SearchPage> {
+bool _voiceOn = false;
+Random random = Random();
+AnimationController _baseController;
+AnimationController _controller;
+
+class _SearchPageState extends State<SearchPage>
+    with SingleTickerProviderStateMixin {
+  @override
+  void initState() {
+    super.initState();
+    _baseController = AnimationController(
+        vsync: this, duration: Duration(milliseconds: 2000));
+    _controller = _baseController..drive(CurveTween(curve: Curves.bounceIn));
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -49,26 +65,46 @@ class _SearchPageState extends State<SearchPage> {
                 child: Stack(
                   alignment: FractionalOffset.center,
                   children: <Widget>[
-                    TextField(
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.white,
-                        hintText: 'Search for',
-                        hintStyle: TextStyle(color: Color(0xFFCCCCCC)),
-                        border: OutlineInputBorder(
-                            borderSide:
-                                BorderSide(width: 0, style: BorderStyle.none),
-                            borderRadius: const BorderRadius.all(
-                              const Radius.circular(10.0),
-                            )),
+                    Visibility(
+                      visible: !_voiceOn,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          hintText: 'Search for',
+                          hintStyle: TextStyle(color: Color(0xFFCCCCCC)),
+                          border: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(width: 0, style: BorderStyle.none),
+                              borderRadius: const BorderRadius.all(
+                                const Radius.circular(10.0),
+                              )),
+                        ),
+                      ),
+                    ),
+                    Visibility(
+                      visible: _voiceOn,
+                      child: SizedBox(
+                        width: 144,
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: _createLines(),
+                        ),
                       ),
                     ),
                     GestureDetector(
+                      onTapCancel: () {
+                        _controller.stop();
+                      },
+                      onTap: () {
+                        _startVoice();
+                      },
                       onLongPressStart: (details) {
-                        print('{{{{{LONGPRESS START}}}}}');
+                        _startVoice();
                       },
                       onLongPressEnd: (details) {
-                        print('{{{{{LONGPRESS END}}}}}');
+                        _endVoice();
                       },
                       child: Container(
                         padding: EdgeInsets.symmetric(horizontal: 20),
@@ -82,11 +118,53 @@ class _SearchPageState extends State<SearchPage> {
                   ],
                 ),
               ),
-              Text('Use the mic for searching or taking an action')
+              Text('Use the mic for searching or taking an action'),
             ],
           ),
         ),
       ),
     );
   }
+
+  List<Widget> _createLines() => List.generate(
+      36,
+      (i) => SizeTransition(
+            sizeFactor: CurvedAnimation(
+                parent: _baseController,
+                curve: SineCurve(count: _getRandom(1, 8).toDouble()),
+                reverseCurve: Curves.slowMiddle),
+            child: Container(
+              width: 2,
+              height: 20,
+              decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.all(Radius.circular(100))),
+            ),
+          ));
+
+  void _startVoice() {
+    _controller.repeat();
+    setState(() {
+      _voiceOn = true;
+    });
+  }
+
+  void _endVoice() {
+    _controller.stop();
+    setState(() {
+      _voiceOn = false;
+    });
+  }
+}
+
+int _getRandom(int min, int max) {
+  return random.nextInt(max) + min;
+}
+
+class SineCurve extends Curve {
+  final double count;
+  SineCurve({this.count});
+
+  @override
+  double transformInternal(double t) => sin(count * 2 * pi * t) * 0.5 + 0.5;
 }
